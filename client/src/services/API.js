471f49497a675d555f7e9a7a9ef706f4b6e39342
Sean Kelliher <sbkelliher@icloud.com>
2022-11-12 11:38:43 -0500

    Remove 'localhost' URL and 'process.env.PORT' for Heroku.

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 export default() => {
     return axios.create({
-        baseURL: "http://localhost:8081/" // Originally backticks.
+        baseURL: process.env.PORT // Originally backticks and http://localhost:8081/.
     });
 };
+
