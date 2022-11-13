c8ac3bfc8cecf706ab420b1ea1a2ccce1c7471e0
Sean Kelliher <sbkelliher@icloud.com>
2022-11-13 12:23:15 -0500

    Modify 'baseURL'. Trying to fix Heroku problems.

--- src/services/API.js
+++ src/services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default() => {
     return axios.create({
-        baseURL: "https://us-national-parks-mongo.herokuapp.com/"
+        baseURL: `${process.env.PORT}`
     });
 };
 
